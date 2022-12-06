6c9e66b3b0cf923085323e8067f115fc06dca662
rahulg3520 <rahul@nestgroup.net>
2022-12-06 11:10:34 +0530

    add course

--- src/main/java/com/example/CourseApp_backend/controller/CourseController.java
+++ src/main/java/com/example/CourseApp_backend/controller/CourseController.java
@@ -1,9 +1,25 @@
 package com.example.CourseApp_backend.controller;
 
+import com.example.CourseApp_backend.model.Course;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CourseController {
+
+    @PostMapping(path = "/add",produces = "application/json",consumes = "application/json")
+    public String addStudent(@RequestBody Course c)
+    {
+        System.out.println(c.toString());
+        System.out.println(c.getCourseTitle().toString());
+        System.out.println(c.getCourseDescription().toString());
+        System.out.println(c.getCourseDuration().toString());
+        System.out.println(c.getCourseDate().toString());
+        System.out.println(c.getCourseVenue().toString());
+
+        return "add student page";
+    }
 
 
 }
