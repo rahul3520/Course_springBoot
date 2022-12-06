483e971a1699911b8a1194fc0ddd5e16bd0c39cd
rahulg3520 <rahul@nestgroup.net>
2022-12-06 10:57:21 +0530

    course controller

---
+++ src/main/java/com/example/CourseApp_backend/controller/CourseController.java
@@ -0,0 +1,9 @@
+package com.example.CourseApp_backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CourseController {
+
+
+}
---
+++ src/test/java/com/example/CourseApp_backend/CourseAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CourseApp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
